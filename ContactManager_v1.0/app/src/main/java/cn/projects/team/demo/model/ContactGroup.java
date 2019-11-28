28805adf66dc77ac23a7b16d185ceaae55067165
KeigoUtdMa <kxm175330@utdallas.edu>
2019-11-27 20:12:54 -0600

    Revert "Add ContactGroup"
    This reverts commit 4e7f3592ea849496949ba285a2499dc26ba9f4fe.

--- ContactManager_v1.0/app/src/main/java/cn/projects/team/demo/model/ContactGroup.java
+++ ContactManager_v1.0/app/src/main/java/cn/projects/team/demo/model/ContactGroup.java
@@ -1,20 +1,17 @@
-//This is a class for group contact
 package cn.projects.team.demo.model;
 
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Generated;
 @Entity
-//group informations will also store in database 
 public class ContactGroup {
     @Id(autoincrement = true)
-    //a group is object and has an id and group name
     private Long id ;
     private String groupName;
     @Generated(hash = 1960676043)
     public ContactGroup(Long id, String groupName) {
-        this.id=id;
-        this.groupName=groupName;
+        this.id = id;
+        this.groupName = groupName;
     }
     @Generated(hash = 1043149959)
     public ContactGroup() {
@@ -23,12 +20,12 @@ public class ContactGroup {
         return this.id;
     }
     public void setId(Long id) {
-        this.id = id;//set id and return id
+        this.id = id;
     }
     public String getGroupName() {
         return this.groupName;
     }
     public void setGroupName(String groupName) {
-        this.groupName = groupName;//set name and return name
+        this.groupName = groupName;
     }
 }
